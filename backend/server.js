ea92ceb90fccb865d4ae6ffa47acfce2823ddfcc
emaan2609 <emaan.official02@gmail.com>
2025-03-26 18:38:00 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://job-find-client.vercel.app");
+  res.header("Access-Control-Allow-Origin", "https://job-find-client.vercel.app/");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", "true");
