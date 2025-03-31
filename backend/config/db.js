85ad4daf6f55c15d6e0e7e34a8b84bb1ca4d0d8f
emaan2609 <emaan.official02@gmail.com>
2025-03-31 18:55:38 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-function connectToDB() {mongoose.connect(process.env.MONGO_URI).then(()=>{
-    console.log("Connected to DB")
-})}
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error);
+    process.exit(1);
+  }
+};
 
-module.exports = connectToDB
+module.exports = connectToDB;
